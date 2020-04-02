311bc311d7d063ba3417da52e87e9fc549dcd996
Mstislav <zvezdsla@inbox.ru>
2020-04-02 12:18:21 +0300

    2 задание

--- script.js
+++ script.js
@@ -1,25 +1,93 @@
-let money = prompt("20000", ''),
-	time = prompt('2020-03-01', '');
+'use strict';
 
+
+let money = +prompt ("Ваш бюджет на месяц?", ""),
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+    
 let appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: false
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
 };
 
-let a1 = prompt("Room", ''),
-	a2 = prompt("10000", ''),
-	a3 = prompt("Eting", ''),
-	a4 = prompt("5000", '');
 
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
+for (let i = 0; i < 2; i++) {
+    let a = prompt ("Введите обязательную статью рассходов в этом месяце", ""),
+        b = prompt ("Во сколько обойдется?", "");
 
-// Важно! В последних версиях браузеров поведение работы со свойствами объектов поменялось. 
-// Теперь синтаксис через точку может не работать. Поэтому выше я использовал квадратные скобки. 
-// Если вы это читаете - то в скором времени обновится и видео в самом курсе. Спасибо за внимание!
+    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
 
-alert(appData.budget / 30);
+        console.log ("done");
+
+        appData.expenses[a] = b;
+    } else {                            
+        console.log ("bad result");
+        i--;
+    }
+
+}
+
+
+
+// для 2 цикла WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью рассходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+
+
+
+// для 3 цикла DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью рассходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+
+alert ("Ежедневный бюджет:" + appData.moneyPerDay + "Что-то");
+
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Высокий уровень достатка");
+} else {
+	console.log ("Произошла ошибка");
+}	
